71809f03381eb1881b47b47ea57943246befc985
Kiernan Geoghegan <kpg184@gmail.com>
2022-12-05 18:13:17 -0500

    updates and examples

---
+++ learn-rust/testing/target/debug/deps/testing-efeb21bfa2a57b4e.d
@@ -0,0 +1,5 @@
+/Users/kiernangeoghegan/Documents/GitHub/ru-sol-project/learn-rust/testing/target/debug/deps/testing-efeb21bfa2a57b4e: src/main.rs
+
+/Users/kiernangeoghegan/Documents/GitHub/ru-sol-project/learn-rust/testing/target/debug/deps/testing-efeb21bfa2a57b4e.d: src/main.rs
+
+src/main.rs:
---
+++ learn-rust/testing/target/debug/testing.d
@@ -0,0 +1,1 @@
+/Users/kiernangeoghegan/Documents/GitHub/ru-sol-project/learn-rust/testing/target/debug/testing: /Users/kiernangeoghegan/Documents/GitHub/ru-sol-project/learn-rust/testing/src/main.rs
